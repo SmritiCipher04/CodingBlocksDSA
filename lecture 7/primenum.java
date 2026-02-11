07688110e5f09c25ebcad5e1fad0ae8c7d86dcf3
smriti1804 <smritisarkare2@gmail.com>
2026-02-11 14:27:52 +0530

    added lecture 7 programs

---
+++ lecture 7/primenum.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class primenum {
+  public static void main(String[] args) {
+      Scanner sc=  new Scanner(System.in);
+      System.out.print("enter a number: ");
+      int n = sc.nextInt();
+      boolean isprime = true;
+      for(int i = 2; i<=n-1; i++){
+        if(n%i == 0){
+          isprime = false;
+          break;
+        }
+        
+      }
+      if(isprime == true){
+        System.out.println("prime");
+      }
+      else{
+        System.out.println("not prime");
+      }
+      sc.close();
+  }
+}

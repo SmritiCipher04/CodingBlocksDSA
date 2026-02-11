f7a20634c5e908aa07ef200ea477fbd03708a40f
smriti1804 <smritisarkare2@gmail.com>
2026-02-11 14:58:44 +0530

    new program added

--- lecture 7/primenum.java
+++ lecture 7/primenum.java
@@ -5,7 +5,7 @@ public class primenum {
       System.out.print("enter a number: ");
       int n = sc.nextInt();
       boolean isprime = true;
-      for(int i = 2; i<=n-1; i++){
+      for(int i = 2; i <= n-1; i++){
         if(n%i == 0){
           isprime = false;
           break;
